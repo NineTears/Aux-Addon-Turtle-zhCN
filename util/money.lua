32f639a2313d28e3f6df8e2ceb9a2cea37ca8995
hellowind <67744274+NineTears@users.noreply.github.com>
2024-11-13 10:22:33 +0800

    汉化

--- util/money.lua
+++ util/money.lua
@@ -3,6 +3,14 @@ module 'aux.util.money'
 local T = require 'T'
 local aux = require 'aux'
 
+GOLD_TEXT_EN = 'g'
+SILVER_TEXT_EN = 's'
+COPPER_TEXT_EN = 'c'
+
+GOLD_TEXT_CN = '金'
+SILVER_TEXT_CN = '银'
+COPPER_TEXT_CN = '铜'
+
 M.GOLD_TEXT = '|cffffd70ag|r'
 M.SILVER_TEXT = '|cffc7c7cfs|r'
 M.COPPER_TEXT = '|cffeda55fc|r'
@@ -30,7 +38,7 @@ function M.to_string2(money, exact, color)
 	local SILVER = 'e6e6e6'
 	local COPPER = 'c8602c'
 	local START = '|cff%s%d' .. FONT_COLOR_CODE_CLOSE
-	local PART = color .. '.|cff%s%02d' .. FONT_COLOR_CODE_CLOSE
+	local PART = color .. ' |cff%s%02d' .. FONT_COLOR_CODE_CLOSE
 	local NONE = '|cffa0a0a0' .. TEXT_NONE .. FONT_COLOR_CODE_CLOSE
 
 	if not exact and money >= COPPER_PER_GOLD then
@@ -40,17 +48,18 @@ function M.to_string2(money, exact, color)
 
 	local str = ''
 
+	local settings = aux.character_data.tooltip
 	local fmt = START
 	if g > 0 then
-		str = str .. format(fmt, GOLD, g)
+		str = str .. format(fmt, GOLD, g) .. (settings.unit_cn and GOLD_TEXT_CN or GOLD_TEXT_EN)
 		fmt = PART
 	end
 	if s > 0 or c > 0 then
-		str = str .. format(fmt, SILVER, s)
+		str = str .. format(fmt, SILVER, s) .. (settings.unit_cn and SILVER_TEXT_CN or SILVER_TEXT_EN)
 		fmt = PART
 	end
 	if c > 0 then
-		str = str .. format(fmt, COPPER, c)
+		str = str .. format(fmt, COPPER, c) .. (settings.unit_cn and COPPER_TEXT_CN or COPPER_TEXT_EN)
 	end
 	if str == '' then
 		str = NONE
@@ -64,10 +73,21 @@ function M.to_string(money, pad, trim, color, no_color)
 	local gold, silver, copper = to_gsc(money)
 
 	local gold_text, silver_text, copper_text
+	local settings = aux.character_data.tooltip
+	local g, s ,c
+	if settings.unit_cn then
+		g = GOLD_TEXT_CN
+		s = SILVER_TEXT_CN
+		c = COPPER_TEXT_CN
+	else 
+		g = GOLD_TEXT_EN
+		s = SILVER_TEXT_EN
+		c = COPPER_TEXT_EN
+	end
 	if no_color then
-		gold_text, silver_text, copper_text = 'g', 's', 'c'
+		gold_text, silver_text, copper_text = g, s, c
 	else
-		gold_text, silver_text, copper_text = GOLD_TEXT, SILVER_TEXT, COPPER_TEXT
+		gold_text, silver_text, copper_text = '|cffffd70a'.. g ..'|r', '|cffc7c7cf'.. s ..'|r', '|cffeda55f'.. c ..'|r'
 	end
 
 	local text
@@ -108,14 +128,26 @@ function M.from_string(value)
 
 	value = gsub(gsub(strlower(value), '|c%x%x%x%x%x%x%x%x', ''), FONT_COLOR_CODE_CLOSE, '')
 
-	local gold = tonumber(aux.select(3, strfind(value, '(%d*%.?%d+)g')))
-	local silver = tonumber(aux.select(3, strfind(value, '(%d*%.?%d+)s')))
-	local copper = tonumber(aux.select(3, strfind(value, '(%d*%.?%d+)c')))
+	local settings = aux.character_data.tooltip
+	local g, s ,c
+	if settings.unit_cn then
+		g = GOLD_TEXT_CN
+		s = SILVER_TEXT_CN
+		c = COPPER_TEXT_CN
+	else 
+		g = GOLD_TEXT_EN
+		s = SILVER_TEXT_EN
+		c = COPPER_TEXT_EN
+	end
+
+	local gold = tonumber(aux.select(3, strfind(value, '(%d*%.?%d+)'..g)))
+	local silver = tonumber(aux.select(3, strfind(value, '(%d*%.?%d+)'..s)))
+	local copper = tonumber(aux.select(3, strfind(value, '(%d*%.?%d+)'..c)))
 	if not gold and not silver and not copper then return end
 
-	value = gsub(value, '%d*%.?%d+g', '', 1)
-	value = gsub(value, '%d*%.?%d+s', '', 1)
-	value = gsub(value, '%d*%.?%d+c', '', 1)
+	value = gsub(value, '%d*%.?%d+'..g, '', 1)
+	value = gsub(value, '%d*%.?%d+'..s, '', 1)
+	value = gsub(value, '%d*%.?%d+'..c, '', 1)
 	if strfind(value, '%S') then return end
 
 	return from_gsc(gold or 0, silver or 0, copper or 0)
